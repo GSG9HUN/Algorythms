ea8254a5461d03057c2efaec4e7d6698f3a99730
GSG9HUN <olahsandorlajos99@gmail.com>
2022-07-30 13:25:18 +0200

    QuickSort implemented

---
+++ SortAlgorithms/QuickSort/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+int[] list = new[] {10,80,30,90,40,50,70 };
+QuickSort.QuickSort sort = new QuickSort.QuickSort(list);
+
+sort.QuickSortAlgorithm(0,list.Length-1);
+
+
+foreach (var element in sort.List)
+{
+    Console.Write(element+" ");
+}
---
+++ SortAlgorithms/QuickSort/QuickSort.cs
@@ -0,0 +1,44 @@
+﻿namespace QuickSort;
+
+public class QuickSort
+{
+    public int[] List { get; }
+
+    public QuickSort(int[] list)
+    {
+        List = list;
+    }
+
+    public void QuickSortAlgorithm(int low, int high)
+    {
+        if (low >= high) return;
+        var pi = Partition(low, high);
+            
+        QuickSortAlgorithm(low,pi-1);
+        QuickSortAlgorithm(pi+1,high);
+
+    }
+
+    private int Partition(int low, int high)
+    {
+        var pivot = List[high];
+        var i = low - 1;
+        int temp;
+        for (var j = low; j <= high; j++)
+        {
+            if (List[j] >= pivot) continue;
+            i++;
+
+            temp = List[i];
+            List[i] = List[j];
+            List[j] = temp;
+        }
+
+        temp = List[i + 1];
+        List[i+1]= List[high];
+        List[high] = temp;
+        
+        return i+1;
+    }
+
+}
